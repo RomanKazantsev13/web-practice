106e6ae55c9067986dd63201dcd47b88bb80a026
RomanKazantsev13 <dj.ka3anova@gmail.com>
2021-03-07 21:01:03 +0300

    Первая страница в Symfony. Добавление недостающих папок

---
+++ homepage/var/cache/dev/twig/75/757cb53a522a3014ba575e9b4232f0011a09397a338e40be0ebd585d2087429c.php
@@ -0,0 +1,241 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* homepage.html.twig */
+class __TwigTemplate_1790fdbf7b6dbad7de8385770e6213beb40706afe090abfd4a8375fdda986e8b extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'stylesheet' => [$this, 'block_stylesheet'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "homepage.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "homepage.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        <link rel=\"shortcut icon\" href=\"Images/favicon.ico\" type=\"image/x-icon\">
+    
+
+        ";
+        // line 9
+        $this->displayBlock('stylesheet', $context, $blocks);
+        // line 13
+        echo "
+    </head>
+    <body>
+    ";
+        // line 16
+        $this->displayBlock('body', $context, $blocks);
+        // line 52
+        echo "    </body>
+</html>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Welcome!";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 9
+    public function block_stylesheet($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheet"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheet"));
+
+        // line 10
+        echo "               <link href=\"CSS/style.css\" rel=\"stylesheet\" type=\"text/css\">
+
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 16
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 17
+        echo "        <header>
+          <p>Сайт-визитка. Обо мне.</p>
+        </header>
+        <article>
+          <h1>Роман Казанцев</h1>
+          <p>Я студент ПС-14. До того, как поступить в Волгатех на это направление я не увлекался программированием и всем, что с этим направлением связано. Несмотря на это, я мне удается хорошо решать различные задачи и получать за это высокие баллы. На мой взгляд самое главное в процессе обучения это мотивация.</p>
+          <img src=\"Images/Roman.jpg\" alt=\"my photo\">
+          <div class=\"my_hobbies\">
+            <h2>Мои увлечения</h2>
+            <ul>
+              <li>Их нет</li>
+              <li>так как</li>
+              <li>все время</li>
+              <li>уходит</li>
+              <li>на учебу</li>
+            </ul>
+            <h2>Мои хобби</h2>
+            <ol>
+              <li>Их тоже нет</li>
+              <li>так как</li>
+              <li>единственное хобби</li>
+              <li>на ближацшие 3,5 года</li>
+              <li>это учеба на ПС</li>
+            </ol>
+          </div>
+          <div class=\"clear\"></div>
+          <div>
+            <span>Хочешь написать мне? Жми на &#8594;</span> 
+            <a href=\"\">VK</a>
+          </div>
+        </article>
+        <footer>
+          <p>Copyright &#169;Frontend 2021</p>
+        </footer>
+    ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "homepage.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  131 => 17,  121 => 16,  109 => 10,  99 => 9,  80 => 5,  68 => 52,  66 => 16,  61 => 13,  59 => 9,  52 => 5,  46 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>{% block title %}Welcome!{% endblock %}</title>
+        <link rel=\"shortcut icon\" href=\"Images/favicon.ico\" type=\"image/x-icon\">
+    
+
+        {% block stylesheet %}
+               <link href=\"CSS/style.css\" rel=\"stylesheet\" type=\"text/css\">
+
+        {% endblock %}
+
+    </head>
+    <body>
+    {% block body %}
+        <header>
+          <p>Сайт-визитка. Обо мне.</p>
+        </header>
+        <article>
+          <h1>Роман Казанцев</h1>
+          <p>Я студент ПС-14. До того, как поступить в Волгатех на это направление я не увлекался программированием и всем, что с этим направлением связано. Несмотря на это, я мне удается хорошо решать различные задачи и получать за это высокие баллы. На мой взгляд самое главное в процессе обучения это мотивация.</p>
+          <img src=\"Images/Roman.jpg\" alt=\"my photo\">
+          <div class=\"my_hobbies\">
+            <h2>Мои увлечения</h2>
+            <ul>
+              <li>Их нет</li>
+              <li>так как</li>
+              <li>все время</li>
+              <li>уходит</li>
+              <li>на учебу</li>
+            </ul>
+            <h2>Мои хобби</h2>
+            <ol>
+              <li>Их тоже нет</li>
+              <li>так как</li>
+              <li>единственное хобби</li>
+              <li>на ближацшие 3,5 года</li>
+              <li>это учеба на ПС</li>
+            </ol>
+          </div>
+          <div class=\"clear\"></div>
+          <div>
+            <span>Хочешь написать мне? Жми на &#8594;</span> 
+            <a href=\"\">VK</a>
+          </div>
+        </article>
+        <footer>
+          <p>Copyright &#169;Frontend 2021</p>
+        </footer>
+    {% endblock %}
+    </body>
+</html>
+", "homepage.html.twig", "C:\\Users\\user\\Desktop\\Volgatech\\web-practice\\homepage\\templates\\homepage.html.twig");
+    }
+}
